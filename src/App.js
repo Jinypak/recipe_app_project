ba89cfb7fcb3a3e6b2bbb190c05cc97342f9de55
Park Jinyong <79570234+Jinypak@users.noreply.github.com>
2022-04-14 14:49:01 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { BrowserRouter, Link } from "react-router-dom";
+import "./App.css";
+import Category from "./components/Category";
+import Search from "./components/Search";
+import Pages from "./pages/Pages";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>
+            <span style={{ fontFamily: "Gothic A1" }}>만국의 레시피</span>
+          </Logo>
+        </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  margin-left: 20px;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
+
+export default App;
